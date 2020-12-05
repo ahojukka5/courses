4b9d9499a4a39cb5042725ed2fb438d9eb80cd83
Jukka Aho <ahojukka5@gmail.com>
2020-12-05 15:01:15 +0700

    Move data to separate file

---
+++ src/data/courses.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Node.js',
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1,
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id: 2,
+      },
+    ],
+  },
+];
--- src/index.js
+++ src/index.js
@@ -5,53 +5,9 @@ import 'semantic-ui-css/semantic.min.css';
 import { Container, Divider, Icon, Header } from 'semantic-ui-react';
 
 import Course from './components/Course';
+import courses from './data/courses';
 
 const App = () => {
-  const courses = [
-    {
-      id: 1,
-      name: 'Half Stack application development',
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: 'Node.js',
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
-
   const rows = () => {
     return courses.map((course) => <Course key={course.id} course={course} />);
   };
